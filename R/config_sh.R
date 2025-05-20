31ed8bcd9bc05eeb43f9fe239f98cb84718e9b02
Emilio Sánchez <emilio.sanchez@posteo.de>
2025-05-20 12:10:31 +0200

    Fix the build of file paths in config.sh

--- R/config_sh.R
+++ R/config_sh.R
@@ -11,15 +11,16 @@
 #' @examples config_sh("C:/Users/esanc/Documents/", "T32ULC", "2020")
 config_sh <- function(directory, tile, year, wcover = "2020"){
 
-  img_folder <- paste0(directory,"/deploy_example/sentinel2/", year, "/",tile,"/")
+  img_folder <- file.path(directory, "deploy_example","sentinel2", year, tile)
+
 
   ### TXT file
   zip_files <- list.files(path = img_folder,
                           pattern = paste0(".*", tile, ".*\\.zip$"),  # Match tile name and end with .zip
                           full.names = FALSE)
 
-  dir.create(paste0(directory,"/deploy_example/image_paths/",year,"/"), recursive=T)
-  output_file <- paste0(directory,"/deploy_example/image_paths/",year,"/",tile, ".txt")
+  dir.create(file.path(directory, "deploy_example","image_paths",year), recursive=T)
+  output_file <- file.path(directory,"deploy_example", "image_paths", year, paste0(tile, ".txt"))
 
   writeLines(zip_files, output_file)
   cat("Created zip file list as text file:", output_file, "with", length(zip_files), "entries.\n")
@@ -29,14 +30,14 @@ config_sh <- function(directory, tile, year, wcover = "2020"){
   ### Configure File
   config_values <- list(
     YEAR = year,
-    DEPLOY_IMAGE_PATH= paste0(img_folder, zip_files[1]),
-    GCHM_DEPLOY_DIR= paste0("./deploy_example/predictions/",year,"/",tile),
-    tile_name=tile,
+    DEPLOY_IMAGE_PATH = file.path(img_folder, zip_files[1]),
+    GCHM_DEPLOY_DIR = file.path("./deploy_example","predictions",year, tile),
+    tile_name = tile,
     wcover = wcover
   )
 
   # Path to the config file
-  config_path <- paste0(directory,"global_config.sh")
+  config_path <- file.path(directory,"global_config.sh")
 
   # Format config lines
   lines <- sapply(names(config_values), function(key) {
